d924d32f0501c24d960a68c36e169a2a5125600b
afaantuh <doodflixp@gmail.com>
2024-12-09 13:43:19 +0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const DOODSTREAM_BASE_URL =
     process.env.DOODSTREAM_BASE_URL || "https://doodapi.com";
-export const DOODSTREAM_API_KEY = process.env.DOODSTREAM_API_KEY || '411362i0upwdslk3rdhxbm';
-export const SITENAME = process.env.SITENAME || "Doodstream Bokep";
+export const DOODSTREAM_API_KEY = process.env.DOODSTREAM_API_KEY || '476305pvyme4tp3yf9fqo0';
+export const SITENAME = process.env.SITENAME || "ABGFLIX - Bokep ABG";
 export const DEFAULT_PER_PAGE = parseInt(process.env.DEFAULT_PER_PAGE || "20");
 export const DEFAULT_REVALIDATE_INTERVAL = 3600;
