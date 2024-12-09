4c0f94204bcfe32803d7cfa433699760a781b5cf
afaantuh <doodflixp@gmail.com>
2024-12-09 23:44:09 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,7 +1,18 @@
 import Script from 'next/script';
 
 const AdScript = () => (
-  <Script src="\/\/februaryinjectionproudly.com\/0a\/04\/86\/0a0486afe9a438a56b5be94d26ae9948.js"/>
+  <script>
+(function(fpeuy){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = fpeuy || {};
+s.src = "\/\/few-politics.com\/cwD\/9C6.b\/2H5_lgSKWxQY9nNMTCgI2\/Nkj\/cR3TN_yA0G1XO\/DxY\/2QNGz\/c\/4k";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
+</script>
 );
 
 export default AdScript;
