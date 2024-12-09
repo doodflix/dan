227e147adeb49feb037f47ecb7981cdce35e48ad
afaantuh <doodflixp@gmail.com>
2024-12-10 00:02:11 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,7 +1,7 @@
 import Script from 'next/script';
 
 const AdScript = () => (
-  <Script src="\/\/few-politics.com\/cwD\/9C6.b\/2H5_lgSKWxQY9nNMTCgI2\/Nkj\/cR3TN_yA0G1XO\/DxY\/2QNGz\/c\/4k"/>
+  <Script src="https://s3.tebi.io/abgflix/adshill.js"/>
 );
 
 export default AdScript;
